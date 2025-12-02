313da047778e5f5628f5b6d942a2c18f6120d2ba
danasiongsin <144954515+danasiongsin@users.noreply.github.com>
2025-12-01 20:57:19 -0500

    trying new conn

--- ITWS2110‐Fall2025‐siongd‐Quiz2/conn.php
+++ ITWS2110‐Fall2025‐siongd‐Quiz2/conn.php
@@ -1,15 +1,16 @@
 <?php
-$host = 'localhost';
-$db   = 'ITWS2110‐Fall2025‐siongd‐Quiz2';
-$user = 'root';
-$pass = 'root';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+$host = "localhost";
+$dbname = "ITWS2110‐Fall2025‐siongd‐Quiz2";
+$username = "phpmyadmin";
+$password = "NewPMApassword123!";
+$dbOK = false;
 
 try {
-    $pdo = new PDO($dsn, $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
 ?>
+
+
